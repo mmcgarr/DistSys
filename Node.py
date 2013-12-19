cb48f1073cf9a139196d256ef8067b819acda498
mmcgarr <mmcgarr@tcd.ie>
2013-12-19 23:56:22 +0000

    Joining Simply

--- Node.py
+++ Node.py
@@ -4,21 +4,26 @@ import json
 from Packet import Packet
 import math 
 from socket import *
+import threading
 
-
+HOST = "127.0.0.1"
 PORT = 8767
 MESSAGE = "OMG HI"
 
 class Node:
 	def __init__(self, node_id):
 		self.node_id = node_id
+		self.routing_table = ([])
 		
-		self.sock = socket(AF_INET, SOCK_DGRAM)
+		self.sendSock = socket(AF_INET, SOCK_DGRAM)
+		self.recieve()
+		print "done with receiving"
 		self.join()
 		
 	def join(self):
+		print 'joining'
 		p = Packet("JOINING_NETWORK_SIMPLIFIED", self.node_id, "127.0.0.1", "")
-		p.send(self.sock)
+		p.send(self.sendSock)
 
 	def leave(self):
 		pass
@@ -29,6 +34,27 @@ class Node:
 			hash = hash *31 + ord(c)
 
 		return math.fabs(hash)
+
+	def listenAndRespond(self):
+		s = socket(AF_INET, SOCK_DGRAM)
+		s.bind((HOST,PORT))
+		addr = (HOST,PORT)
+		while True:
+			data, addr = s.recvfrom(1024)
+			packet = json.loads(data)
+			if packet["type"] == "JOINING_NETWORK_SIMPLIFIED":
+				print "Add node to routing table"
+				self.routing_table.append((packet["node_id"], packet["from_ip"]))
+				print self.routing_table
+
+
+	def recieve(self):
+		t = threading.Thread(target = self.listenAndRespond)
+#		t.daemon = True
+		t.start()
+
+
+
 
 def main():
 	Node(42)
--- Packet.py
+++ Packet.py
@@ -3,21 +3,32 @@ import json
 
 class Packet:
 
-	def __init__(self, type, node_id, from_ip_address, to_ip_address = none, data):
+	def __init__(self, type, node_id=None, from_ip = None, to_ip = None, gateway_id = None, route_table = None, target_id = None):
 		self.type = type
-		self.node_id = node_id
-		self.from_ip_address = from_ip_address
-		self.to_ip_address = to_ip_address
-		self.data = data
 
-		if type == "JOINING_NETWORK_SIMPLIFIED"
-				
+		if type == "JOINING_NETWORK_SIMPLIFIED":
+			self.node_id = node_id
+			self.target_id = target_id
+			self.from_ip = from_ip
+		elif type == "JOINING_NETWORK_RELAY_SIMPLIFIED":
+			self.node_id = node_id
+			self.target_id = target_id
+			self.gateway_id = gateway_id
+		elif type == "ROUTING_INFO":
+			self.gateway_id = gateway_id
+			self.node_id = node_id
+			self.ip_address = from_ip
+			self.table = route_table
+		elif type == "LEAVING NETWORK":
+			self.node_id = node_id
+		elif type == "INDEX":
+			self.target_id = target_id
+			self.sender_id = sender_id
+
 
 	def send(self, sock):
 		print "sending"
-		
-		sock.sendto(json.dumps(self.__dict__), (self.to_ip_address, 8767))
-
+		sock.sendto(json.dumps(self.__dict__), ("127.0.0.1", 8767)) 
 
 class SearchResult:
 	pass
